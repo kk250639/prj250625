437afe100585a2e19fe596e848aecb6f91c6577a
kk250639 <chesyeo@gmail.com>
2025-06-25 09:14:28 +0900

    first commit

---
+++ src/main/java/com/example/prj250625/Prj250625Application.java
@@ -0,0 +1,13 @@
+package com.example.prj250625;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj250625Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prj250625Application.class, args);
+    }
+
+}
